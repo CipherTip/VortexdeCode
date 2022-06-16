b455fc7db28a0d430f40a1886da6a025423de33a
Kiran <Ciphertip@gmail.com>
2022-06-16 13:46:42 +0530

    Data access Layer generic repository pattern with Unit Of Work for more scalable entity repository connection with db

---
+++ VortexdeCodeDL/Services/IRepositoryService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using VortexdeCodeDL.Data;
+using VortexdeCodeDL.RepositoryBase;
+
+namespace VortexdeCodeDL.Services
+{
+    internal interface IRepositoryService
+    {
+        VortexDBContext DbContext { get; set; }
+
+        IGenericRepositoryBase<T> GetGenericRepository<T>() where T : class;
+
+        T GetCustomRepository<T>(Func<VortexDBContext, object> factory = null) where T : class;
+    }
+}
---
+++ VortexdeCodeDL/UnitOfWork/OsUnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VortexdeCodeDL.Data;
+using VortexdeCodeDL.RepositoryBase;
+using VortexdeCodeDL.Services;
+
+namespace VortexdeCodeDL.UnitOfWork
+{
+    internal class OsUnitOfWork : IOsUnitOfWork
+    {
+        private readonly VortexDBContext _dbContext;
+        private readonly IRepositoryService _repositoryService;
+
+        public OsUnitOfWork(VortexDBContext dbContext)
+        {
+            _dbContext = dbContext;
+
+            if (_repositoryService == null)
+            {
+                _repositoryService = new RepositoryServiceImpl();
+            }
+
+            _repositoryService.DbContext = _dbContext;
+        }
+
+        int IOsUnitOfWork.Save()
+        {
+            return _dbContext.SaveChanges();
+        }
+
+        Task<int> IOsUnitOfWork.SaveAsync()
+        {
+            return _dbContext.SaveChangesAsync();
+        }
+
+        void IDisposable.Dispose()
+        {
+            _dbContext.Dispose();
+        }
+
+        IGenericRepositoryBase<T> IOsUnitOfWork.GetRepository<T>()
+        {
+            return _repositoryService.GetGenericRepository<T>();
+        }
+    }
+}
